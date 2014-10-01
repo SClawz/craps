806179f7025f6c2619850f8476d05faa0235176a
Zane <zzglobicki@gmail.com>
2014-10-01 16:38:40 -0400

    Improved the variable names

--- craps.py
+++ craps.py
@@ -1,26 +1,26 @@
 from random import randint
 
 print('come-out')
-r1 = randint(1, 6) # These are bad variables, as r1 and r2 mean little to the programmer
-r2 = randint(1, 6)
-v = r1 + r2        # This means nothing to me
-print(v)
-if v == 7 or v == 11:
+firstRoll = randint(1, 6) 
+secondRoll = randint(1, 6)
+rollSum = firstRoll + secondRoll        
+print(rollSum)
+if rollSum == 7 or rollSum == 11:
     print('You win!')
-elif v == 2 or v == 3 or v == 12:
+elif rollSum == 2 or rollSum == 3 or rollSum == 12:
     print('Craps, you lose.')
 else:
-    print('The point is', v)
-    r1 = randint(1, 6)
-    r2 = randint(1, 6)
-    w = r1 + r2         # What's this?
-    print(w)
-    while w != v and w != 7:
-        r1 = randint(1, 6)
-        r2 = randint(1, 6)
-        w = r1 + r2
-        print(w)
-    if w == 7:
+    print('The point is', rollSum)
+    firstRoll = randint(1, 6)
+    secondRoll = randint(1, 6)
+    rollSum2 = firstRoll + secondRoll         
+    print(rollSum2)
+    while rollSum2 != rollSum and rollSum2 != 7:
+        firstRoll = randint(1, 6)
+        secondRoll = randint(1, 6)
+        rollSum2 = firstRoll + secondRoll
+        print(rollSum2)
+    if rollSum2 == 7:
         print('Seven out, you lose.')
     else:
         print('Pass, you win!')
